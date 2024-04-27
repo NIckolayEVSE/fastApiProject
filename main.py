e07e10aac9d1c9fdfabf8b6c1a8e6dcad716c1dd
NIckolayEVSE <biomaster2014@yandex.ru>
2024-04-27 22:00:26 +0300

    .

--- main.py
+++ main.py
@@ -25,5 +25,5 @@ async def check_data_endpoint(phone: str):
     return {"address": address}
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", reload=True)
